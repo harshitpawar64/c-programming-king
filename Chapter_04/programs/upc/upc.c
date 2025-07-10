a36730c54ab6f4c7f0d7479138bbcf601aac11b6
Harshit Pawar <harshitpawar2007@outlook.com>
2025-07-10 21:19:51 +0530

    Add Chapter 4 programs

---
+++ Chapter_04/programs/upc/upc.c
@@ -0,0 +1,37 @@
+/************************************************************************
+ * Name: upc.c                                                          *
+ * Purpose: Computes the Universal Product Code (UPC) check digit.      *
+ * Author: Harshit Pawar                                                *
+ * Date: 2025-07-09                                                     *
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(void)
+{
+    int type, m1, m2, m3, m4, m5, p1, p2, p3, p4, p5;
+
+    // Asks user for first digit of UPC (Type)
+    printf("First digit (Type): ");
+    scanf("%1d", &type);
+
+    // Asks user for first group of 5 digits of UPC (Manufacturer Prefix)
+    printf("First group of 5 digits (Manufacturer): ");
+    scanf("%1d%1d%1d%1d%1d", &m1, &m2, &m3, &m4, &m5);
+
+    // Asks user for second group of 5 digits of UPC (Product Item number)
+    printf("Second group of 5 digits (Product): ");
+    scanf("%1d%1d%1d%1d%1d", &p1, &p2, &p3, &p4, &p5);
+
+    // Computes UPC check digit
+    int first_sum = type + m2 + m4 + p1 + p3 + p5;
+    int second_sum = m1 + m3 + m5 + p2 + p4;
+    int total = (first_sum * 3) + second_sum;
+    int check_digit = 9 - (total - 1) % 10;
+
+    // Outputs computed UPC check digit
+    printf("Check digit: %d\n", check_digit);
+
+    // Return status code '0'
+    return 0;
+}
